f89936dfcdd4d6a54371919d513abb295217aa7e
Jakub Sedláček <kubka.sedlacek04@gmail.com>
2021-10-10 20:57:56 +0200

    Add Files

---
+++ src/components/DataTitle.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="text-center">
+        Selected: <h2 class="text-2xl text-indigo-800 font-bold"> {{text}} </h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DataTitle',
+    props: ['text', 'dataDate'],
+}
+</script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer class="cursor-default">
+        <div>
+            <h2 class="text-xl text-center  p-10 md:mt-32"> Made by <a href="https://jakubsedlacek.xyz" target="_blank" class="font-bold text-indigo-700">Jakub Sedláček</a> with ❤️</h2>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
