be9c816cbe4d10974f4603d007ef8de8d05351ad
Jakub Sedláček <kubka.sedlacek04@gmail.com>
2021-10-10 21:02:52 +0200

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="cursor-default">
         <div>
-            <h2 class="text-xl text-center  p-10 md:mt-32"> Made by <a href="https://jakubsedlacek.xyz" target="_blank" class="font-bold text-indigo-700">Jakub Sedláček</a> with ❤️</h2>
+            <h2 class="text-xl text-center md:mt-40"> Made by <a href="https://jakubsedlacek.xyz" target="_blank" class="font-bold text-indigo-700">Jakub Sedláček</a> with ❤️</h2>
         </div>
     </footer>
 </template>
